a19424f39e907a44c1f90ab12286effc66257ecb
Arunkarthi <arunkarthi827@gmail.com>
2023-05-06 22:56:42 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -1,20 +1,23 @@
-const mongoose = require('mongoose')     //Third party module
-const express = require('express')     //Third party module
-const auth = require('./routes/autentication.js')
-const app = express()
-app.use(express.json())
-require('dotenv').config();
-app.use("/api/auth",routes)
-app.use("/api/admin",routes)
-mongoose.connect(process.env.link).then(()=>{
-    console.log('succ')
-}).catch((err)=>{
-    console.log(err)
-})
+const mongoose = require("mongoose"); //Third party module
+const express = require("express"); //Third party module
+const auth = require("./routes/autentication.js");
+const app = express();
+app.use(express.json());
+require("dotenv").config();
 
-app.use("/api/auth",auth)
-app.use("/api/user",booking)
+mongoose
+  .connect(process.env.link)
+  .then(() => {
+    console.log("succ");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.listen(3000,(req,res)=>{
-    console.log("Connected succesfully");
-})
+app.use("/api/auth", auth);
+app.use("/api/admin", routes);
+app.use("/api/user", booking);
+
+app.listen(3000, (req, res) => {
+  console.log("Connected succesfully");
+});
